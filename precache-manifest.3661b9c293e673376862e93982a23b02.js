3d053d4bfa20048c7d3e85769ee09548e1719d99
Michael Douglas <michaeldouglas@MacBook-Air-de-Michael.local>
2020-08-13 12:22:20 -0300

    Updates

---
+++ precache-manifest.3661b9c293e673376862e93982a23b02.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19b0764512cca67720b1c09a397bcc28",
+    "url": "/curriculoWeb/index.html"
+  },
+  {
+    "revision": "dedfbab8615b04c2dc32",
+    "url": "/curriculoWeb/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "3acef2c9059204489a79",
+    "url": "/curriculoWeb/static/css/main.18a97eb2.chunk.css"
+  },
+  {
+    "revision": "dedfbab8615b04c2dc32",
+    "url": "/curriculoWeb/static/js/2.4da49652.chunk.js"
+  },
+  {
+    "revision": "825e3c71abfed54ead16b6fbe34cbf8a",
+    "url": "/curriculoWeb/static/js/2.4da49652.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3acef2c9059204489a79",
+    "url": "/curriculoWeb/static/js/main.16f2db06.chunk.js"
+  },
+  {
+    "revision": "9671962fde90a441c914",
+    "url": "/curriculoWeb/static/js/runtime-main.d9977c59.js"
+  },
+  {
+    "revision": "7f5529bb5a22f0b68d69b01c8dcfebf4",
+    "url": "/curriculoWeb/static/media/Louis George Cafe Bold Italic.7f5529bb.ttf"
+  },
+  {
+    "revision": "37931f1a4b21c268033168ae2c816b08",
+    "url": "/curriculoWeb/static/media/Louis George Cafe Bold.37931f1a.ttf"
+  },
+  {
+    "revision": "794a3b6488660007d19d60d395cbf508",
+    "url": "/curriculoWeb/static/media/Louis George Cafe Italic.794a3b64.ttf"
+  },
+  {
+    "revision": "f0cf41f8e86201301bd510d55f1963d7",
+    "url": "/curriculoWeb/static/media/Louis George Cafe Light Italic.f0cf41f8.ttf"
+  },
+  {
+    "revision": "c1f3cc2ba08804503e6546c2ffcd5bd0",
+    "url": "/curriculoWeb/static/media/Louis George Cafe Light.c1f3cc2b.ttf"
+  },
+  {
+    "revision": "a617b62cd8089465865a37c676da00e2",
+    "url": "/curriculoWeb/static/media/Louis George Cafe.a617b62c.ttf"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/curriculoWeb/static/media/logo.5d5d9eef.svg"
+  }
+]);
